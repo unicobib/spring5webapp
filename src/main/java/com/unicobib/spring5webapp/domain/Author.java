c0e8076716ba4f75adfa5db233a07eb0c77920ed
unicobib <bibek.125060@gmail.com>
2020-09-29 01:42:25 +0530

    adding mapping for book and authors

--- src/main/java/com/unicobib/spring5webapp/domain/Author.java
+++ src/main/java/com/unicobib/spring5webapp/domain/Author.java
@@ -1,11 +1,16 @@
 package com.unicobib.spring5webapp.domain;
 
+import javax.persistence.*;
 import java.util.Set;
 
+@Entity
 public class Author {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String firstName;
     private String lastName;
+    @ManyToMany(mappedBy = "authors")
     private Set<Book> books;
 
     public Author() {
--- src/main/java/com/unicobib/spring5webapp/domain/Book.java
+++ src/main/java/com/unicobib/spring5webapp/domain/Book.java
@@ -1,10 +1,17 @@
 package com.unicobib.spring5webapp.domain;
 
+import javax.persistence.*;
 import java.util.Set;
 
+@Entity
 public class Book {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String bookName;
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
+    inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Author> authors;
 
     public Book() {
